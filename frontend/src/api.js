2439714b823ee33a4f49fe7c14ea8f05e78cfb62
Mmarrianna9 <mmarrianna9@gmail.com>
2026-07-24 22:09:54 +0200

    Configurazione Axios

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://new-hair-style-backend.onrender.com/api'
 });
 
 export default api;
--- frontend/src/chatApi.js
+++ frontend/src/chatApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const chatApi = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://new-hair-style-chatbot.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
